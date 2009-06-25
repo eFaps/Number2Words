035ade705ee7c3b08acf9167c11a99cd3851f16d
tim.moxter <tim.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-06-25 22:32:49 +0000

    - use for the tests data provider so that each value is in the test protocoll

--- number2words/trunk/src/test/java/org/efaps/number2words/test/Tests.java
+++ number2words/trunk/src/test/java/org/efaps/number2words/test/Tests.java
@@ -25,6 +25,7 @@ import java.util.Locale;
 import org.efaps.number2words.Converter;
 import org.efaps.number2words.IConverter;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -39,10 +40,28 @@ public class Tests
 {
     /**
      * Test the convert for English language.
+     *
+     * @param _number   number to check
+     * @param _text     expected conversion
      */
-    public void english()
+    @Test(dataProvider = "english")
+    public void english(final int _number,
+                        final String _text)
     {
-        final Object[][] data = new Object[][] {
+        final IConverter converter = Converter.getConverter(Locale.ENGLISH);
+        Assert.assertEquals(converter.convert(_number), _text, "test '" + _number + "'");
+    }
+
+    /**
+     * Returns the English test data as an array of an array of integer and the
+     * related converted text.
+     *
+     * @return English test data
+     */
+    @DataProvider(name = "english")
+    public Object[][] englishTestData()
+    {
+        return new Object[][] {
             {0, "zero"},
             {1, "one"},
             {2, "two"},
@@ -60,24 +79,41 @@ public class Tests
             {800, "eight hundred"},
             {801, "eight hundred one"},
             {1316, "one thousand three hundred sixteen"},
+            {700000, "seven hundred thousand"},
+            {900000, "nine hundred thousand"},
             {1000000, "one million"},
             {2000000, "two million"},
             {3000200, "three million two hundred"},
-            {700000, "seven hundred thousand"},
-            {900000, "nine hundred thousand"},
             {123456789, "one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine"},
             {2147483647, "two billion one hundred forty seven million "
                         + "four hundred eighty three thousand six hundred forty seven"}
         };
-        this.test(data, Locale.ENGLISH);
     }
 
     /**
-     * Tests the test for Spanish language.
+     * Tests the convert for Spanish language.
+     *
+     * @param _number   number to check
+     * @param _text     expected conversion
      */
-    public void spanish()
+    @Test(dataProvider = "spanish")
+    public void spanish(final int _number,
+                        final String _text)
     {
-        final Object[][] data = new Object[][] {
+        final IConverter converter = Converter.getConverter(new Locale("es"));
+        Assert.assertEquals(converter.convert(_number), _text, "test '" + _number + "'");
+    }
+
+    /**
+     * Returns the Spanish test data as an array of an array of integer and the
+     * related converted text.
+     *
+     * @return Spanish test data
+     */
+    @DataProvider(name = "spanish")
+    public Object[][] spanishTestData()
+    {
+        return new Object[][] {
             {0, "cero"},
             {1, "uno"},
             {2, "dos"},
@@ -90,38 +126,22 @@ public class Tests
             {16, "diecis\u00E9is"},
             {100, "cien"},
             {118, "ciento dieciocho"},
+            {147, "cientos cuarenta y siete"},
             {200, "dos cientos"},
             {219, "dos cientos diecinueve"},
             {800, "ocho cientos"},
             {801, "ocho cientos uno"},
             {1316, "un mil tres cientos diecis\u00E9is"},
             {24124, "veinte quatro mil veinte quatro"},
-            {1000000, "un mill\u00F3n"},
-            {2000000, "dos bill\u00F3nes"},
-            {3000200, "tres bill\u00F3nes dos cientos"},
             {700000, "siete cientos mil"},
             {900000, "nueve cientos mil"},
+            {1000000, "un mill\u00F3n"},
+            {2000000, "dos mill\u00F3nes"},
+            {3000200, "tres mill\u00F3nes dos cientos"},
             {123456789, "veinte tres bill\u00F3nes quatro cientos cincuenta y seis mil siete cientos ochenta y nueve"},
-            {2147483647, "dos mill\u00F3nes cuarenta y siete bill\u00F3nes quatro cientos ochenta "
-                        + "y tres mil seis cientos cuarenta y siete"},
+            {1000000000, "un bill\u00F3n"},
+            {2147483647, "dos bill\u00F3nes cientos cuarenta y siete mill\u00F3nes quatro cientos ochenta "
+                        + "y tres mil seis cientos cuarenta y siete"}
         };
-        this.test(data, new Locale("es"));
-    }
-
-    /**
-     * Tests that for given data the number to word conversion is correct. The
-     * <code>_data</code> is an array of an array of integer and the related
-     * converted text.
-     *
-     * @param _data     data array to test
-     * @param _locale   locale of the data to test
-     */
-    private void test(final Object[][] _data,
-                      final Locale _locale)
-    {
-        final IConverter converter = Converter.getConverter(_locale);
-        for (final Object[] dataLine : _data)  {
-            Assert.assertEquals(converter.convert((Integer) dataLine[0]), (String) dataLine[1]);
-        }
     }
 }
