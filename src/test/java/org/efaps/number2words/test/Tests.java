24de7e70682909073d930033c09f58117396f146
tim.moxter <tim.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-06-25 20:55:51 +0000

    - finish the test cases including java documentation

--- src/test/java/org/efaps/number2words/test/Tests.java
+++ src/test/java/org/efaps/number2words/test/Tests.java
@@ -22,39 +22,106 @@ package org.efaps.number2words.test;
 
 import java.util.Locale;
 
-import org.testng.annotations.Test;
-
 import org.efaps.number2words.Converter;
 import org.efaps.number2words.IConverter;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
- * TODO comment!
+ * Implements the test cases for English and Spanish conversion of numbers to
+ * word.
  *
  * @author The eFaps Team
  * @version $Id$
  */
+@Test()
 public class Tests
 {
-    @Test
-    public void english() {
-       System.out.println("English");
-       final long[] numbers = new long[]{0, 1,2,3,4,5,6,7,8,16,100,118,200,219,800,801,1316,1000000,2000000,3000200,700000,900000,123456789,2147483647};
-       final IConverter converter = Converter.getConverter(Locale.ENGLISH);
-       for (final long number : numbers) {
-           System.out.println("    " + number + ": " + converter.convert(number));
-       }
-
+    /**
+     * Test the convert for English language.
+     */
+    public void english()
+    {
+        final Object[][] data = new Object[][] {
+            {0, "zero"},
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"},
+            {6, "six"},
+            {7, "seven"},
+            {8, "eight"},
+            {16, "sixteen"},
+            {100, "one hundred"},
+            {118, "one hundred eighteen"},
+            {200, "two hundred"},
+            {219, "two hundred nineteen"},
+            {800, "eight hundred"},
+            {801, "eight hundred one"},
+            {1316, "one thousand three hundred sixteen"},
+            {1000000, "one million"},
+            {2000000, "two million"},
+            {3000200, "three million two hundred"},
+            {700000, "seven hundred thousand"},
+            {900000, "nine hundred thousand"},
+            {123456789, "one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine"},
+            {2147483647, "two billion one hundred forty seven million "
+                        + "four hundred eighty three thousand six hundred forty seven"}
+        };
+        this.test(data, Locale.ENGLISH);
     }
 
+    /**
+     * Tests the test for Spanish language.
+     */
+    public void spanish()
+    {
+        final Object[][] data = new Object[][] {
+            {0, "cero"},
+            {1, "uno"},
+            {2, "dos"},
+            {3, "tres"},
+            {4, "quatro"},
+            {5, "cinco"},
+            {6, "seis"},
+            {7, "siete"},
+            {8, "ocho"},
+            {16, "diecis\u00E9is"},
+            {100, "cien"},
+            {118, "ciento dieciocho"},
+            {200, "dos cientos"},
+            {219, "dos cientos diecinueve"},
+            {800, "ocho cientos"},
+            {801, "ocho cientos uno"},
+            {1316, "un mil tres cientos diecis\u00E9is"},
+            {24124, "veinte quatro mil veinte quatro"},
+            {1000000, "un mill\u00F3n"},
+            {2000000, "dos bill\u00F3nes"},
+            {3000200, "tres bill\u00F3nes dos cientos"},
+            {700000, "siete cientos mil"},
+            {900000, "nueve cientos mil"},
+            {123456789, "veinte tres bill\u00F3nes quatro cientos cincuenta y seis mil siete cientos ochenta y nueve"},
+            {2147483647, "dos mill\u00F3nes cuarenta y siete bill\u00F3nes quatro cientos ochenta "
+                        + "y tres mil seis cientos cuarenta y siete"},
+        };
+        this.test(data, new Locale("es"));
+    }
 
-    @Test
-    public void spanish() {
-       System.out.println("Spanish");
-       final long[] numbers = new long[]{0, 1,2,3,4,5,6,7,8,16,100,118,200,219,800,801,1316, 24124,1000000,2000000,3000200,700000,900000,123456789,2147483647};
-       final IConverter converter = Converter.getConverter(new Locale("es"));
-       for (final long number : numbers) {
-           System.out.println("    " + number + ": " + converter.convert(number));
-       }
-
+    /**
+     * Tests that for given data the number to word conversion is correct. The
+     * <code>_data</code> is an array of an array of integer and the related
+     * converted text.
+     *
+     * @param _data     data array to test
+     * @param _locale   locale of the data to test
+     */
+    private void test(final Object[][] _data,
+                      final Locale _locale)
+    {
+        final IConverter converter = Converter.getConverter(_locale);
+        for (final Object[] dataLine : _data)  {
+            Assert.assertEquals(converter.convert((Integer) dataLine[0]), (String) dataLine[1]);
+        }
     }
 }
