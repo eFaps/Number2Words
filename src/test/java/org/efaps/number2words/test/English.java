23b9c1dd7807fa660f41240c8f446d399e8baa19
tim.moxter <tim.moxter@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2009-06-26 07:28:19 +0000

    - the English and Spanish tests are separated into two classes

--- src/test/java/org/efaps/number2words/test/English.java
+++ src/test/java/org/efaps/number2words/test/English.java
@@ -29,20 +29,19 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
- * Implements the test cases for English and Spanish conversion of numbers to
- * word.
+ * Implements the test cases for English conversion of numbers to words.
  *
  * @author The eFaps Team
  * @version $Id$
  */
 @Test()
-public class Tests
+public class English
 {
     /**
      * Test the convert for English language.
      *
      * @param _number   number to check
-     * @param _text     expected conversion
+     * @param _text     expected conversion in English
      */
     @Test(dataProvider = "english")
     public void english(final int _number,
@@ -87,76 +86,6 @@ public class Tests
             {123456789, "one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine"},
             {2147483647, "two billion one hundred forty seven million "
                         + "four hundred eighty three thousand six hundred forty seven"}
-        };
-    }
-
-    /**
-     * Tests the convert for Spanish language.
-     *
-     * @param _number   number to check
-     * @param _text     expected conversion
-     */
-    @Test(dataProvider = "spanish")
-    public void spanish(final int _number,
-                        final String _text)
-    {
-        final IConverter converter = Converter.getConverter(new Locale("es"));
-        Assert.assertEquals(converter.convert(_number), _text, "test '" + _number + "'");
-    }
-
-    /**
-     * Returns the Spanish test data as an array of an array of integer and the
-     * related converted text.
-     *
-     * @return Spanish test data
-     */
-    @DataProvider(name = "spanish")
-    public Object[][] spanishTestData()
-    {
-        return new Object[][] {
-            {0, "cero"},
-            {1, "uno"},
-            {2, "dos"},
-            {3, "tres"},
-            {4, "quatro"},
-            {5, "cinco"},
-            {6, "seis"},
-            {7, "siete"},
-            {8, "ocho"},
-            {16, "diecis\u00E9is"},
-            {21, "veintiuno"},
-            {22, "veintid\u00F3s"},
-            {23, "veintitr\u00E9s"},
-            {24, "veinticuatro"},
-            {25, "veinticinco"},
-            {26, "veintis\u00E9is"},
-            {27, "veintisiete"},
-            {28, "veintiocho"},
-            {29, "veintinueve"},
-            {30, "treinta"},
-            {100, "cien"},
-            {118, "ciento dieciocho"},
-            {122, "ciento veintid\u00F3s"},
-            {147, "ciento cuarenta y siete"},
-            {200, "doscientos"},
-            {219, "doscientos diecinueve"},
-            {500, "quinientos"},
-            {800, "ochocientos"},
-            {801, "ochocientos uno"},
-            {1316, "un mil trescientos diecis\u00E9is"},
-            {24124, "veinticuatro mil ciento veinticuatro"},
-            {700000, "sietecientos mil"},
-            {900000, "novecientos mil"},
-            {1000000, "un mill\u00F3n"},
-            {2000000, "dos mill\u00F3nes"},
-            {3000200, "tres mill\u00F3nes doscientos"},
-            {123456789, "ciento veintitr\u00E9s mill\u00F3nes "
-                        + "quatrocientos cincuenta y seis mil "
-                        + "sietecientos ochenta y nueve"},
-            {1000000000, "un bill\u00F3n"},
-            {2147483647, "dos bill\u00F3nes ciento cuarenta y siete mill\u00F3nes "
-                        + "quatrocientos ochenta y tres mil "
-                        + "seiscientos cuarenta y siete"}
         };
     }
 }
