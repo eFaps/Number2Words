ab0e039803d3cb197a75c95746faa4df763950cb
Jan Moxter <jan.moxter@moxter.net>
2025-06-12 22:23:15 -0500

    Java 17

--- src/main/java/org/efaps/number2words/IConverter.java
+++ src/main/java/org/efaps/number2words/IConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2020 The eFaps Team
+ * Copyright © 2003 - 2024 The eFaps Team (-)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-
 package org.efaps.number2words;
 
 /**
